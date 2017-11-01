4c2e9bdec2d17ec49bfc7e275b37a92fceabe194
JunhaoWang <wangjh2@mcmaster.ca>
2017-11-01 19:47:13 -0400

    Fixed Errors from accidentally deleting MapController

---
+++ Assets/Resources/Map/Tests/SystemTest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class SystemTest : IPrebuildSetup
+{
+
+    public void Setup()
+    {
+        SceneManager.LoadScene("Map");
+    }
+
+
+//#################Tests Expected to PASS###############################
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator MainMenuStartGame() {
+        yield return null;
+    }
+    [UnityTest]
+    public IEnumerator EachNodeThreeNeighbours()
+    {
+        
+        yield return null;
+    }
+ 
+    //--------------Private methods
+ 
+
+    //#################Tests Expected to FAIL###############################
+ 
+
+
+
+
+
+}
