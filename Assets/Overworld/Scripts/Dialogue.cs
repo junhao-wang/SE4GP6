39dc1d99b0dd10902877923b42a341f8e7861112
yangy82 <yangy82@mcmaster.ca>
2017-11-27 02:33:12 -0500

    etc

---
+++ Assets/Overworld/Scripts/Dialogue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[Serializable]
+
+public class Dialogue{
+
+    public string name;
+    public string image;
+    public string side;
+    public List<string> Lines;
+}
