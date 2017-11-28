bef23cb074921810fcdd566bf9fb81771030ce0e
Castagner Gabriel <castaggj@mcmaster.ca>
2017-11-27 19:33:19 -0500

    ItemMenu is added, files re arranged
    The menu is hardcoded right now but will be fixed soon wihen based on
    panel information

---
+++ Assets/UI/Scripts/LevelInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelInfo : MonoBehaviour {
+			
+
+
+
+	private Sprite level;
+	private Image levelRenderer;
+	public int levelnum;
+	//private Object[] levelRenderer;
+	// Use this for initialization
+	void Awake () {
+
+
+
+
+		Sprite[] levels = Resources.LoadAll <Sprite>("Icons/levels");
+		levelnum = Random.Range (0, 4);
+		level = levels[levelnum];
+		levelRenderer = gameObject.GetComponent <Image>();
+		levelRenderer.sprite = level;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//levelRenderer.sprite = levels[level];
+	}
+
+	int getlevel(){
+		return levelnum;
+	}
+
+}
