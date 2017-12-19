a038a150b83ab5701aa3a7b8a3d6b119e255f682
JunhaoWang <wangjh2@mcmaster.ca>
2017-12-19 14:22:55 -0500

    Meta files changed

--- Assets/Overworld/Map/Scripts/PartyProperties.cs
+++ Assets/Overworld/Map/Scripts/PartyProperties.cs
@@ -7,6 +7,7 @@ public class PartyProperties : MonoBehaviour {
     public GameObject OccupiedNode;
     public GameObject Canvas;
     public List<GameObject> path;
+    public List<int> CompletedDialogue = new List<int>();
     public bool inDialogue = false;
     public float initialMoney = 15, initialSupply = 15;
     public enum ResourceType {MONEY,SUPPLY ,SIZE};
