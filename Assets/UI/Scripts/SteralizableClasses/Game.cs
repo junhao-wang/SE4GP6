9f1ea7625e82fac3ad3775ab9c63db05419ad6ae
Castagner Gabriel <castaggj@mcmaster.ca>
2017-12-19 17:31:04 -0500

    Finalized itemMenu scene, ready to be imported to main game

---
+++ Assets/UI/Scripts/SteralizableClasses/Game.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Game {
+
+
+
+	public static Game current;
+	public HeroStats Kroner;
+	public HeroStats Lee;
+	public HeroStats Alexei;
+
+	public Game(){
+		Kroner = new HeroStats ("Kroner");
+		Lee = new HeroStats ("Lee");
+		Alexei = new HeroStats ("Alexei");
+
+	}
+
+}
